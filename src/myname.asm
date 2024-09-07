c44c6c324e06b6ab04a9d0c348e91e949c0b948c
valesskablanco <valesskablanco@estudiantec.cr>
2024-09-07 17:27:55 -0600

    rotation and mirroring working

--- src/myname.asm
+++ src/myname.asm
@@ -7,60 +7,46 @@ jmp start;
 %define START_X 100 ; starting x position
 %define START_Y 100 ; starting y position
 %define LETTER_WIDTH 3
-%define DEGREES 360
+%define DEGREES 4
 
 start:
-    mov bx, 0 ; Init degrees register
-
     cli ; clr all interrumpts
     push 0xA000 ; video memory graphics segment
     pop es ; extra segments from stack pop them
     xor di, di ; set register to 0
     xor ax, ax ; set register to 0
-    mov ax, 0x05 ; 320x200 resolution 4 colors mode
+    mov ax, 0x13 ; 320x200 resolution 4 colors mode
     int 0x10 ; video interrumpt
 
 
 start_loop:
     call start_message
 
+    mov si, 0 ; Init mirror register counter
+    mov bx, 0 ; Init degrees register counter
+
     mov ah, 0      ; Set up for keyboard input
     int 16h        ; BIOS interrupt for keyboard input
 
     cmp ah, 1Ch    ; Enter key
-    je init_write_names
+    je print_game
 
     jmp start_loop
 
-init_write_names:
-    ; clear screen
-    mov ah, 0x6
-    mov al, 0x0
-    mov bh, BLACK
-    mov dl, 39
-    mov dh, 24
-    int 0x10
-    
-    ; writing names in random position for the first time
-    mov ah, 0xc ; write pixel
-    mov al, WHITE ; color WHITE
-    mov bh, 0x00 ; let page to 0
-    mov cx, START_X ; x position
-    mov dx, START_Y ; y position
+print_game:
+    call clear_screen
 
-    call plot_V_90 ; TODO: write complete names
-
-    jmp game_loop
+    jmp write_names_by_key
 
 game_loop:
-    ; Check for keyboard input without waiting
-    mov ah, 1      ; Function 1: check if a key is pressed
-    int 16h        ; Call BIOS keyboard interrupt
-    jz game_loop  ; If no key is pressed, continue loop
-    
-    ; If a key is pressed, read it
-    mov ah, 0      ; Function 0: read key and get scan code in AH
-    int 16h        ; Call BIOS keyboard interrupt
+
+    mov ah, 01h    ; Check for keystroke
+    int 16h        ; BIOS interrupt to check for keystroke
+
+    jz game_loop      ; Jump if no key is available
+
+    mov ah, 0      ; Set up for keyboard input
+    int 16h        ; BIOS interrupt for keyboard input
     
     cmp ah, 01h    ; ESC key
     je finish_game
@@ -80,8 +66,10 @@ game_loop:
     ; If no arrow key was pressed, go back to start of the loop
     jmp game_loop
 
+
 rotate_left:
-    add bx, 90     ; Add 90 degrees when left arrow is pressed
+    mov si, 0
+    add bx, 1     ; Add 90 degrees when left arrow is pressed
     cmp bx, DEGREES
     jl skip_reset  ; Skip reset if under 360 degrees
     sub bx, DEGREES  ; Reset rotation if over 360
@@ -89,7 +77,8 @@ rotate_left:
     jmp skip_reset
 
 rotate_right:
-    sub bx, 90     ; Subtract 90 degrees when right arrow is pressed
+    mov si, 0
+    sub bx, 1     ; Subtract 90 degrees when right arrow is pressed
     cmp bx, 0
     jge skip_reset ; Skip reset if degrees >= 0
     add bx, DEGREES   ; Reset to 360 if negative
@@ -97,48 +86,50 @@ rotate_right:
     jmp skip_reset
 
 mirror_up:
-    jmp finish_game
+    mov si, 1
+    mov bx, 0
+    jmp skip_reset
 
 mirror_down:
-    jmp finish_game
+    mov si, 1
+    mov bx, 1  
+    jmp skip_reset
 
 skip_reset:
-    call write_names_by_rotation
-    jmp game_loop
+    call clearkeyboardbuffer
+    jmp print_game
 
+write_names_by_key:
+
+    cmp si, 0
+    je write_names_by_rotation      ; Plot with rotation
+    
+    cmp si, 1
+    je write_names_by_mirroring     ; Plot with mirroring
 
 write_names_by_rotation:
-    ; clear screen
-    mov ah, 0x6
-    mov al, 0x0
-    mov bh, BLACK
-    mov dl, 39
-    mov dh, 24
-    int 0x10
 
-    mov ah, 0xc ; write pixel
-    mov al, WHITE ; color WHITE
-    mov bh, 0x00 ; let page to 0
-    mov cx, START_X ; x position
-    mov dx, START_Y ; y position
-
-    ; TODO: change these for the corresponding rotated write_names
     cmp bx, 0
-    je plot_V_0      ; Plot V with 0-degree rotation
+    je write_names_0      ; Plot with 0-degree rotation
     
-    cmp bx, 90
-    je plot_V_90     ; Plot V with 90-degree rotation
+    cmp bx, 1
+    je write_names_90     ; Plot with 90-degree rotation
 
-    cmp bx, 180
-    je plot_V_180     ; Plot V with 180-degree rotation
+    cmp bx, 2
+    je write_names_180     ; Plot with 180-degree rotation
 
-    cmp bx, 270
-    je plot_V_270     ; Plot V with 270-degree rotation
+    cmp bx, 3
+    je write_names_270     ; Plot with 270-degree rotation
 
-    ret
+write_names_by_mirroring:
+    cmp bx, 0
+    je write_names_up      ; Plot with mirroring up
+    
+    cmp bx, 1
+    je write_names_down     ; Plot with mirroring down
 
 
-write_names:
+write_names_0:
     mov ah, 0xc ; write pixel
     mov al, WHITE ; color WHITE
     mov bh, 0x00 ; let page to 0
@@ -147,22 +138,63 @@ write_names:
 
     call plot_V_0
 
-    ; Move to next letter position
-    add cx, LETTER_WIDTH
-    mov dx, START_Y
+    jmp game_loop
+
+write_names_90:
+    mov ah, 0xc ; write pixel
+    mov al, WHITE ; color WHITE
+    mov bh, 0x00 ; let page to 0
+    mov cx, START_X ; x position
+    mov dx, START_Y ; y position
+
+    call plot_V_90
+
+    jmp game_loop
+
+write_names_180:
+    mov ah, 0xc ; write pixel
+    mov al, WHITE ; color WHITE
+    mov bh, 0x00 ; let page to 0
+    mov cx, START_X ; x position
+    mov dx, START_Y ; y position
+
+    call plot_V_180
+
+    jmp game_loop
+
+write_names_270:
+    mov ah, 0xc ; write pixel
+    mov al, WHITE ; color WHITE
+    mov bh, 0x00 ; let page to 0
+    mov cx, START_X ; x position
+    mov dx, START_Y ; y position
+
+    call plot_V_270
+
+    jmp game_loop
+
+write_names_up:
+    mov ah, 0xc ; write pixel
+    mov al, WHITE ; color WHITE
+    mov bh, 0x00 ; let page to 0
+    mov cx, START_X ; x position
+    mov dx, START_Y ; y position
+
     call plot_A
 
-    ; Move to next letter position
-    add cx, LETTER_WIDTH
-    mov dx, START_Y
+    jmp game_loop
+
+write_names_down:    
+    mov ah, 0xc ; write pixel
+    mov al, WHITE ; color WHITE
+    mov bh, 0x00 ; let page to 0
+    mov cx, START_X ; x position
+    mov dx, START_Y ; y position
+
     call plot_L
 
-    ; Move to next letter position
-    add cx, LETTER_WIDTH
-    mov dx, START_Y
-    ;call plot_E
+    jmp game_loop
 
-    jmp finish_game
 
 plot_V_0:
     ; Plotting points for 'V'
@@ -340,6 +372,12 @@ plot_pixel:
     int 0x10 ; video interrupt
     ret
 
+clearkeyboardbuffer:
+    mov ah,0ch
+    mov al,0
+    int 21h
+    ret
+
 start_message:
     mov ah, 0x2       ; BIOS.SetCursorPosition
     mov bh, 0         ; page 0
@@ -459,6 +497,17 @@ write_message:
     mov ah, 0x0E       ; BIOS.Teletype (in AH)
     mov bh, 0
     mov al, 'R'    ; Point SI to the start of the string
+    int 0x10
+
+    ret
+
+clear_screen:
+    ; clear screen
+    mov ah, 0x6
+    mov al, 0x0
+    mov bh, BLACK
+    mov cx, 0x0000  ; Upper left corner (0,0)
+    mov dx, 0x184F  ; Lower right corner (79,24)
     int 0x10
 
     ret
